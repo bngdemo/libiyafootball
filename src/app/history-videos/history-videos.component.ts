354849cf5a1bd4bf2d33428c75dda38dc2e09e48
bngdemo <49154180+bngdemo@users.noreply.github.com>
2021-09-20 21:04:20 +0530

    Add files via upload

---
+++ src/app/history-videos/history-videos.component.ts
@@ -0,0 +1,37 @@
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history-videos',
+  templateUrl: './history-videos.component.html',
+  styleUrls: ['./history-videos.component.css']
+})
+export class HistoryVideosComponent implements OnInit {
+
+  videos = [
+    {
+      title: 'أريد أن أتناول العشاء مع ميسي',
+      description: 'جلس كريستيانو رونالدو وليونيل ميسي بجانب بعضهما البعض',
+      src: 'assets/videos/1_p.mp4',
+    },
+    {
+      title: 'أريد أن أتناول العشاء مع ميسي',
+      description: 'جلس كريستيانو رونالدو وليونيل ميسي بجانب بعضهما البعض',
+      src: 'assets/videos/2_p.mp4',
+    },
+    {
+      title: 'أريد أن أتناول العشاء مع ميسي',
+      description: 'جلس كريستيانو رونالدو وليونيل ميسي بجانب بعضهما البعض',
+      src: 'assets/videos/1_p.mp4',
+    }
+  ]
+
+  constructor(private location: Location) { }
+
+  ngOnInit(): void {
+  }
+
+  goBackPage() {
+    this.location.back();
+  }
+}
